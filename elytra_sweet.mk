e36469634ea55f02f27c97de8147efc6c62fb39f
Sayooj Soman <80192443+sayooxx@users.noreply.github.com>
2022-07-08 10:42:43 +0530

    Update elytra_sweet.mk

--- elytra_sweet.mk
+++ elytra_sweet.mk
@@ -11,10 +11,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit common Cherish OS  Stuff.
-$(call inherit-product, vendor/elytra/config/common_full_phone.mk)
+
 TARGET_BOOT_ANIMATION_RES := 1080
-PRODUCT_NAME := elytra_sweet
+PRODUCT_NAME := sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
@@ -23,7 +22,9 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Elytra OS Official
-ELYTRA_BUILD_TYPE := OFFICIAL
-USE_GAPPS := true
 TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_USES_BLUR := true
+WITH_GMS := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+CUSTOM_BUILD_TYPE := Official
